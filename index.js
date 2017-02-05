2b9433f885a422264ff049a8bf5f3a21761516bb
togana <o.togana@gmail.com>
2017-02-06 01:41:23 +0900

    add comment

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+/**
+ * merge-descriptors
+ */
+
+/**
+ * Merge the property descriptors of `src` into `dest`
+ *
+ * @param {Object} dest - Object to add descriptors to
+ * @param {Object} src - Object to clone descriptors from
+ * @param {Boolean} [redefine=true] - Redefine `dest` properties with `src` properties
+ * @return {Object} - Reference to dest
+ * @public
+ *
+ * @example
+ * const src = { get name() { return 'togana'; } };
+ * const dest = {};
+ * merge(dest, src);
+ * // dest.name = 'togana'
+ */
 function merge(dest, src, redefine = true) {
   if (!dest) throw new TypeError('dist is unexpected argument.');
   if (!src) throw new TypeError('src is unexpected argument.');
@@ -9,4 +28,9 @@ function merge(dest, src, redefine = true) {
   return dest;
 }
 
+/**
+ * Module exports.
+ *
+ * @public
+ */
 module.exports = merge;
